f619a397db24d3bbb00b0eba0f55418f0c60fd7c
DhruvJha707 <dhruvjha707@gmail.com>
2026-05-02 11:46:46 +0530

    Initial commit

---
+++ lab questions/q23.c
@@ -0,0 +1,59 @@
+/*Reverse a singly linked list (iterative method)*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main() {
+    struct node *head = NULL, *temp = NULL, *newNode;
+    struct node *prev = NULL, *curr = NULL, *nextNode = NULL;
+    int n, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    for (i = 0; i < n; i++) {
+        newNode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    curr = head;
+
+    while (curr != NULL) {
+        nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    head = prev;
+
+    temp = head;
+    printf("Reversed Linked list: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+    return 0;
+}
