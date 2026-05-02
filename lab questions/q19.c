fb9242e07792b0ef7b472bede13492aaec74dcfb
DhruvJha707 <dhruvjha707@gmail.com>
2026-05-02 11:46:21 +0530

    Initial commit

---
+++ lab questions/q19.c
@@ -0,0 +1,65 @@
+/*Delete the last node.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main() {
+    struct node *head = NULL, *temp = NULL, *prev = NULL, *newNode;
+    int n, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    for (i = 0; i < n; i++) {
+        newNode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    if (head == NULL) {
+        printf("List is empty\n");
+        return 0;
+    }
+
+    if (head->next == NULL) {
+        free(head);
+        head = NULL;
+    } else {
+        temp = head;
+        while (temp->next != NULL) {
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = NULL;
+        free(temp);
+    }
+
+    temp = head;
+    printf("Updated Linked list: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+    return 0;
+}
