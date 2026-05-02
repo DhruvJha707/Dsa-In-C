79d42aa23e9bf82247502a9cfb405e8f2f478449
DhruvJha707 <dhruvjha707@gmail.com>
2026-05-02 11:43:09 +0530

    Initial commit

---
+++ lab questions/q3.c
@@ -0,0 +1,40 @@
+/*Write a C program to demonstrate the concept of pointers, pointer arithmetic, and their relationship with arrays. Print the values and addresses of variables and perform operations using pointer increment and dereferencing.*/
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    clock_t start, end;
+    double execution_time;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    if (n <= 0 || n > 100) {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    start = clock();
+
+    printf("Array elements are:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    end = clock();
+
+    execution_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("\nExecution time: %f seconds\n", execution_time);
+
+    return 0;
+}
+
